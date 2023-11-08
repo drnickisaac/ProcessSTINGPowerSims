474e444d6c66b5331d8eba4eff461310aad1e31b
Nick Isaac <njbisaac@gmail.com>
2023-11-08 16:09:22 +0000

    v0.4.6 introducting a constraint on site-level random effect

--- R/defineModel_SS.R
+++ R/defineModel_SS.R
@@ -19,8 +19,8 @@ defineModel_SS <- function(inclPhenology = TRUE,
     }}
 
     ######################### state model priors
-    for(j in 1:nsite) {eta[j] ~ dnorm(0, tau.eta)} # site-level random effect
-    tau.eta ~ T(dt(0, 1, 1), 0, Inf) # Half Cauchy
+    for(j in 1:nsite) {eta[j] ~ dnorm(0, sd=sd.eta)} # site-level random effect
+    sd.eta ~ T(dt(0, 1, 1), 0, 10) # constrained
     Trend ~ dnorm(0, tau=0.0001)
 
     ######################### Obs model
