da3e7c28017716e62874a2d1301cebf5e865c071
Nick Isaac <njbisaac@gmail.com>
2024-01-30 13:56:09 +0000

    v0.9.12 gamma.1 and alpha.1 constrained to be positive

--- R/defineModel_SS.R
+++ R/defineModel_SS.R
@@ -72,16 +72,16 @@ defineModel_SS <- function(incl2ndTransect = TRUE,
     if(inclPanTrap){
       alpha.0 ~ dnorm(-2, tau = 0.0001) # logit detection probability per pan trap at peak phenology (or mean across year).
       if(inclPhenology){
-        #alpha.1 ~ T(dt(0, 1, 1), 0, Inf)
-        alpha.1 ~ dnorm(2, tau = 0.0001)
+        alpha.1 ~ T(dt(0, 1, 1), 0, Inf) # constrained to be positive
+        #alpha.1 ~ dnorm(2, tau = 0.0001)
         } # scaling parameter for detection on pan trap
     }
     gamma.0 ~ dnorm(-2, tau = 0.0001) # detection probability GLM on transects at peak phenology
-    #gamma.1 ~ T(dt(0, 1, 1), 0, Inf) # scaling factor for detection on transects with seasonality
-    gamma.1 ~ dnorm(2, tau = 0.0001) # detection probability GLM on transects at peak phenology
 
 
     if(inclPhenology){
+      #gamma.1 ~ dnorm(2, tau = 0.0001) # detection probability GLM on transects at peak phenology
+      gamma.1 ~ T(dt(0, 1, 1), 0, Inf) # # constrained to be positive
       beta1 ~ dunif(50, 300) # peak detectability/activity. Not constrained to fall within the field season (c(100, 250))
       beta2 ~ T(dt(0, 1, 1), 10, 200) # Half Cauchy. Stdev of phenology. At sd=500 the curve is entirely flat
     }
